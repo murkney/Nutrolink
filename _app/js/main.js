9b58610cf8e4bacd2e4d979b695b17d9a9d6ca1f
murkney <36377174+murkney@users.noreply.github.com>
2023-04-28 02:57:35 +0200

    Importing main products

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,5 +1,7 @@
 import Navigation from "./modules/navigation.js";
 import Slideshow from "./modules/slideshow.js";
+import ProductList from "./modules/main-products.js";
 
 Navigation();
 Slideshow();
+ProductList();
