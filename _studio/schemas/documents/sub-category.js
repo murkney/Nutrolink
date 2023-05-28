a489a1bc4bf9799666ed50d9c46d1265ecadb34f
murkney <36377174+murkney@users.noreply.github.com>
2023-05-29 01:26:26 +0200

    added sub category js for sanity

---
+++ _studio/schemas/documents/sub-category.js
@@ -0,0 +1,20 @@
+export default {
+	title: 'Sub Category',
+	name: 'subcategory',
+	type: 'document',
+	fields: [
+		{
+			title: 'Title',
+			name: 'title',
+			type: 'string'
+		},
+		{
+			title: 'Slug',
+			name: 'slug',
+			type: 'slug',
+			options: {
+				source: 'title'
+			}
+		}
+	]
+};
