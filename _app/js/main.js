20bfd5e90564fb70171dcd1a372f27059538b9d6
murkney <36377174+murkney@users.noreply.github.com>
2023-05-29 21:38:51 +0200

    Added functions

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,7 +1,18 @@
 import Navigation from "./modules/navigation.js";
 import Slideshow from "./modules/slideshow.js";
 import ProductList from "./modules/main-products.js";
+import renderProductPage from "./modules/render-product.js";
+import supplementList from "./modules/supplements.js";
+import performanceList from "./modules/performance.js";
+import { readURL } from "./util/read-url.js";
 
 Navigation();
 Slideshow();
 ProductList();
+// renderProductPage();
+supplementList();
+performanceList();
+
+if (readURL() !== undefined) {
+	renderProductPage()
+}
