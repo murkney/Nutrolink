e607a4632f878f312ace97150c57a761eb07cab8
murkney <36377174+murkney@users.noreply.github.com>
2023-05-30 01:15:13 +0200

    Added more to DOMcreater

--- _app/js/modules/render-product.js
+++ _app/js/modules/render-product.js
@@ -11,40 +11,58 @@ export default async function renderProductPage() {
 		'slug': slug.current,
 		"image": image.asset->url,
 		price,
-		description
+		description,
+		ingridients
 	}`;
 
 	const staticProductItems = await sanity.fetch(query);
 
 	function createProductContainerDOM() {
 		for (const staticProductItem of staticProductItems) {
+			// console.log(staticProductItem);
 			const staticProductContainer = document.getElementById('product-container');
 			
 			const staticProduct = document.createElement('div');
-			const staticProductTitle = document.createElement('h2');
 			const staticProductImage = document.createElement('img');
+			const staticProductTitle = document.createElement('h2');
+			const staticProductDivider = document.createElement('section');
+			const staticProductDetails = document.createElement('section');
+			const staticProductDescription = document.createElement('p');
 			const staticProductPrice = document.createElement('h2');
-			const staticProductAddCart = document.createElement('i');
+			const staticProductAddCart = document.createElement('button');
 			
 			staticProduct.className = 'static-product';
 			staticProductTitle.className = 'static-product__title';
+			staticProductDivider.className = 'static-product__devider';
 			staticProductImage.className = 'static-product__image';
+			staticProductDetails.className = 'static-product__details';
+			staticProductDescription.className = 'static-product__description';
 			staticProductPrice.className = 'static-product__price';
 			staticProductAddCart.className = 'bx bx-shopping-bag static-product__add';
 
-			// staticProduct.href = `/product/?${product.slug}`;
-
 			staticProductTitle.innerText = staticProductItem.title;
 			staticProductImage.src = staticProductItem.image;
+			staticProductDescription.innerText = staticProductItem.description;
 			staticProductPrice.innerText = `${staticProductItem.price},- kr`;
+			// staticProductAddCart.href = ;
 
 			staticProductContainer.appendChild(staticProduct);
+
 			staticProduct.append(
 				staticProductTitle,
-				staticProductImage,
-				staticProductPrice,
+				staticProductDivider,
 				staticProductAddCart
-			);	
+			);
+			
+			staticProductDivider.append(
+				staticProductImage,
+				staticProductDetails
+			);
+
+			staticProductDetails.append(
+				staticProductDescription,
+				staticProductPrice
+			);
 		};	
 	};
 
