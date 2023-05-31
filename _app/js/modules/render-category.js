2dda884653e84f10c10be88c2f8baf62df6b8ee6
MarkusVaa <36377174+murkney@users.noreply.github.com>
2023-05-31 22:00:22 +0200

    Feature/category static (#13)
    * Added static category and subcategory
    
    * removed js
    
    * Added static category and subcategory

---
+++ _app/js/modules/render-category.js
@@ -0,0 +1,56 @@
+import { sanity } from "../sanity.js";
+import { readURL } from "../util/read-url.js";
+
+export default async function renderCategoryPage() {
+	const slug = readURL();
+	const query = `*[_type == 'product' && category->slug.current == '${slug}'] {
+		_id,
+		title,
+		'slug': slug.current,
+		'image': image.asset->url,
+		price,
+		description
+	}`;
+
+	const staticCategoryItems = await sanity.fetch(query);
+
+	function createCategoryContainerDOM() {
+		for (const staticCategoryItem of staticCategoryItems) {
+
+			const categoryContainer = document.getElementById('category-container');
+			
+			const staticCategoryProductItem = document.createElement('a');
+			const staticCategoryProductTitle = document.createElement('h2');
+			const staticCategoryProductImage = document.createElement('img');
+			const staticCategoryProductPrice = document.createElement('h2');
+			const staticCategoryProductAddCart = document.createElement('i');
+			
+			staticCategoryProductItem.className = 'product-item';
+			staticCategoryProductTitle.className = 'product-item__title';
+			staticCategoryProductImage.className = 'product-item__image';
+			staticCategoryProductPrice.className = 'product-item__price';
+			staticCategoryProductAddCart.className = 'bx bx-shopping-bag product-item__add';
+
+			staticCategoryProductItem.href = `/product/?${staticCategoryItem.slug}`;
+			staticCategoryProductTitle.innerText = staticCategoryItem.title;
+			staticCategoryProductImage.src = staticCategoryItem.image;
+			staticCategoryProductPrice.innerText = `${staticCategoryItem.price},- kr`;
+
+			categoryContainer.appendChild(staticCategoryProductItem);
+
+			staticCategoryProductItem.append(
+				staticCategoryProductTitle,
+				staticCategoryProductImage,
+				staticCategoryProductPrice,
+				staticCategoryProductAddCart
+			);
+		};		
+	};
+
+	function renderHTML() {
+		createCategoryContainerDOM();
+	};
+
+	renderHTML();
+
+}
